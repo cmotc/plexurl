d2aef10ad4bad328717a8bda61bbac69829141f1
Daniel Hoherd <hoherd@yelp.com>
2017-03-19 09:36:54 -0700

    Versions

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3",
         "Intended Audience :: End Users/Desktop",
         "Intended Audience :: System Administrators",
